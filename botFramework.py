9710d98f005cc059bc6660146a1caab09be537e2
Brandon Robertiello <77466123+brobertiello@users.noreply.github.com>
2022-11-13 07:28:07 -0600

    updated

--- botFramework.py
+++ botFramework.py
@@ -1,58 +1,74 @@
 import discord
 import os
 
+#DISCORD CLIENT INITIATION
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+#CHECKS IF CHANNEL IS A DM
 async def check(channel):
     if isinstance(channel, discord.channel.DMChannel):
         return True
     else:
         return False
 
+#SENDS DM TO USER
 async def send_dm(member: discord.Member, content):
     channel = await member.create_dm()
     await channel.send(content)
 
+#CONSOLE LOG
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+#WHEN MESSAGE IS RECIEVED
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if await check(message.channel):
+
         #IF IN DMS
+
+        #RESET BOT MEMORY
         if str(message.content).startswith("!reset"):
             await message.channel.send("Resetting")
-            #RESET CODE
+
+        #CLEAR CONVERSATION HISTORY
         elif str(message.content).startswith("!clear"):
             await message.channel.send("Clearing")
-            #CLEAR CONVERSATION
             async for m in message.channel.history():
                 if m.author == client.user:
                     await m.delete()
+
+        #PRINT BOT INFO
         elif str(message.content).startswith("!info"):
             await message.channel.send("This bot was made for **HackUTD IX** by **Brandon Robertiello** and **Nate Reese**!")
-            #BOT INFO
+
+        #PRINT HELP INFO
         elif str(message.content).startswith("!help"):
             await message.channel.send("**Help Menu**")
-            #HELP INFO
             await message.channel.send("**!reset** to reset memory\n**!clear** to clear conversation\n**!info** to see bot information")
+        
+        #AI GENERATED RESPONSE
         else:
             await message.channel.send("This would be my AI generated response")
             #FILL WITH OPENAI CODE
+
     else:
+
         #IF IN REGULAR CHANNEL
         if client.user.mentioned_in(message):
 
             users = [user_mentioned for user_mentioned in message.mentions]
-            users.remove(client.user)
+            if(client.user in users):
+                users.remove(client.user)
 
+            #IF MULTIPLE USERS MENTIONED
             if(len(users)>0):
 
                 for u in users:
@@ -60,25 +76,21 @@ async def on_message(message):
 
                 await message.delete()
 
+            #IF ONLY DOCTORBOT MENTIONED
             else:
 
                 user = message.author
                 user_id = user.id
 
-                #DELETE ALL PREVIOUS DMS
-                #for ch in client.private_channels:
-                #    if await check(ch):
-                #        messages = ch.history()
-                #        async for m in messages:
-                #            if m.author == client.user:
-                #                await m.delete()
-
                 await send_dm(message.author, f"*you mentioned DoctorBot in another server, use **!help** for commands*\n\nHello, <@{user_id}>!\nMy name is **DoctorBot** and I'm here to assist you!\nHow are you feeling today?")
                 await message.delete()
 
+#DISCORD BOT TOKEN
 client.run('token')
 
-#PERMISSIONS
-#READ MESSAGES / VIEW CHANNEL
-#SEND MESSAGES
-#MANAGE MESSAGES
+#PERMISSIONS REQUIRED
+    #READ MESSAGES / VIEW CHANNEL
+    #SEND MESSAGES
+    #MANAGE MESSAGES
+
+#https://discord.com/api/oauth2/authorize?client_id=1041083747900084335&permissions=11264&scope=bot
