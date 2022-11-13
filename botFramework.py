55f606fb4c3460f9475f78cde9e131e075443fb6
Brandon Robertiello <77466123+brobertiello@users.noreply.github.com>
2022-11-13 02:38:03 -0600

    framework file for bot
    need a new bot token that will be stored locally

---
+++ botFramework.py
@@ -0,0 +1,71 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+async def check(channel):
+    if isinstance(channel, discord.channel.DMChannel):
+        return True
+    else:
+        return False
+
+async def send_dm(member: discord.Member, content):
+    channel = await member.create_dm()
+    await channel.send(content)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if await check(message.channel):
+        #IF IN DMS
+        if str(message.content).startswith("!reset"):
+            await message.channel.send("Resetting")
+            #RESET CODE
+        elif str(message.content).startswith("!clear"):
+            await message.channel.send("Clearing")
+            #CLEAR CONVERSATION
+            async for m in message.channel.history():
+                if m.author == client.user:
+                    await m.delete()
+        elif str(message.content).startswith("!info"):
+            await message.channel.send("This bot was made for **HackUTD IX** by **Brandon Robertiello** and **Nate Reese**!")
+            #BOT INFO
+        elif str(message.content).startswith("!help"):
+            await message.channel.send("**Help Menu**")
+            #HELP INFO
+            await message.channel.send("**!reset** to reset memory\n**!clear** to clear conversation\n**!info** to see bot information")
+        else:
+            await message.channel.send("This would be my AI generated response")
+            #FILL WITH OPENAI CODE
+    else:
+        #IF IN REGULAR CHANNEL
+        if client.user.mentioned_in(message):
+            user = message.author
+            user_id = user.id
+
+            #DELETE ALL PREVIOUS DMS
+            #for ch in client.private_channels:
+            #    if await check(ch):
+            #        messages = ch.history()
+            #        async for m in messages:
+            #            if m.author == client.user:
+            #                await m.delete()
+
+            await send_dm(message.author, f"*you mentioned DoctorBot in another server, use **!help** for commands*\n\nHello, <@{user_id}>!\nMy name is **DoctorBot** and I'm here to assist you!\nHow are you feeling today?")
+            await message.delete()
+
+client.run('MTA0MTI0MDA5NTM5NjcyODg2Mg.G9CmEd.C0X-P5dzlvyeBKpSonnSOf6IK1IwhpAZ3IiF5Y')
+
+#PERMISSIONS
+#READ MESSAGES / VIEW CHANNEL
+#SEND MESSAGES
+#MANAGE MESSAGES
