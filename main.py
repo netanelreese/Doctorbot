0c9c08a9535dd8cd75d8797e96bd21425aedc5ad
Nathanael <ngreese7@ou.edu>
2022-11-12 19:12:04 -0600

    Added some shit

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ client = discord.Client(intents = intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-stdPrompt = "You are an AI therapist named DoctorBot. Respond as if you were a therapist.\n\n"
+stdPrompt = "You are an AI therapist named DoctorBot. Respond as if you were a therapist. \n\n"
 
 memory = stdPrompt
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 @client.event
 async def on_message(message):
@@ -30,21 +30,25 @@ async def on_message(message):
         print(memory)
         return
 
-    memory += "You: " + str(message.content) + "\n"
+    if str(message.content).startswith("!reset"):
+        memory = stdPrompt
+        return
+
+    memory += "You: " + str(message.content) + "\nDoctorBot: "
 
     response = openai.Completion.create(
       model="text-davinci-002",
-      prompt= memory + "DoctorBot: ",
+      prompt= memory,
       temperature=0.95,
-      max_tokens=245,
+      max_tokens=500,
       top_p=1.0,
-      frequency_penalty=0.5,
-      presence_penalty=0.0
+      frequency_penalty=2.0,
+      presence_penalty=1.0
     )
 
     json_object = json.loads(str(response))
-    reply = json_object['choices'][0]['text'][2:]
-    memory += "DoctorBot: " + reply + "\n"
+    reply = json_object['choices'][0]['text']
+    memory += reply + "\n"
     await message.channel.send(reply)
 
-client.run('MTA0MTA4Mzc0NzkwMDA4NDMzNQ.G8OJVD.j9M0h4xEjgZt0Q6jFLz6rOXTLcLAALp4M4qwNc')
+client.run('MTA0MTA4Mzc0NzkwMDA4NDMzNQ.G4Y44v.1Tv4sJmgQabbUziG0nZnTDBpPx-9ErXlxrmAKw')
